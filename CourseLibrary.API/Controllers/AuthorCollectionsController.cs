770cc0d1131c0eba6ce860e70e76e4793fa1161a
Sina Abbaszadeh <sinaabszde83@gmail.com>
2025-03-07 20:32:33 +0330

    feat: add a controller and an endpoint for creating a collection of authors

---
+++ CourseLibrary.API/Controllers/AuthorCollectionsController.cs
@@ -0,0 +1,35 @@
+using AutoMapper;
+using CourseLibrary.API.Entities;
+using CourseLibrary.API.Models;
+using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseLibrary.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthorCollectionsController : ControllerBase
+{
+    private ICourseLibraryRepository _courseLibraryRepository;
+    private IMapper _mapper;
+
+    public AuthorCollectionsController(ICourseLibraryRepository courseLibraryRepository, IMapper mapper)
+    {
+        _courseLibraryRepository = courseLibraryRepository ??
+                                   throw new ArgumentNullException(nameof(courseLibraryRepository));
+        _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+    }
+
+    public async Task<ActionResult<IEnumerable<AuthorDto>>> CreateAuthorCollection(IEnumerable<AuthorForCreationDto> authorCollection)
+    {
+        var authorEntities = _mapper.Map<IEnumerable<Author>>(authorCollection);
+        foreach (var author in authorEntities)
+        {
+            _courseLibraryRepository.AddAuthor(author);
+        }
+
+        await _courseLibraryRepository.SaveAsync();
+
+        return Ok();
+    }
+}
