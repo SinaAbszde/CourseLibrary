b88f68b4e2706835f1c3f682ffba85b955179f14
Sina Abbaszadeh <sinaabszde83@gmail.com>
2025-03-14 20:08:02 +0330

    feat: add support for data shaping single resources

--- CourseLibrary.API/Controllers/AuthorsController.cs
+++ CourseLibrary.API/Controllers/AuthorsController.cs
@@ -106,7 +106,7 @@ public class AuthorsController : ControllerBase
     }
 
     [HttpGet("{authorId}", Name = "GetAuthor")]
-    public async Task<ActionResult<AuthorDto>> GetAuthor(Guid authorId)
+    public async Task<IActionResult> GetAuthor(Guid authorId, string? fields)
     {
         // get author from repo
         var authorFromRepo = await _courseLibraryRepository
@@ -118,7 +118,7 @@ public class AuthorsController : ControllerBase
         }
 
         // return author
-        return Ok(_mapper.Map<AuthorDto>(authorFromRepo));
+        return Ok(_mapper.Map<AuthorDto>(authorFromRepo).ShapeData(fields));
     }
 
     [HttpPost]
---
+++ CourseLibrary.API/Helpers/ObjectExtensions.cs
@@ -0,0 +1,54 @@
+﻿using System.Dynamic;
+using System.Reflection;
+
+namespace CourseLibrary.API.Helpers;
+
+public static class ObjectExtensions
+{
+    public static ExpandoObject ShapeData<TSource>(this TSource source, string? fields)
+    {
+        if (source is null)
+        {
+            throw new ArgumentNullException(nameof(source));
+        }
+        
+        ExpandoObject dataShapedObject = new();
+
+        if (string.IsNullOrWhiteSpace(fields))
+        {
+            // all public properties should be in the expando object
+            var propertyInfos =
+                typeof(TSource).GetProperties(BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
+            
+            foreach (var propertyInfo in propertyInfos)
+            {
+                var propertyValue = propertyInfo.GetValue(source);
+
+                ((IDictionary<string, object?>)dataShapedObject).Add(propertyInfo.Name, propertyValue);
+            }
+            
+            return dataShapedObject;
+        }
+
+        var fieldsSplit = fields.Split(',');
+
+        foreach (var field in fieldsSplit)
+        {
+            var propertyName = field.Trim();
+
+            var propertyInfo = typeof(TSource).GetProperty(propertyName,
+                BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
+
+            if (propertyInfo is null)
+            {
+                throw new Exception($"Property {propertyName} wasn't found in type {typeof(TSource)}");
+            }
+            
+            var propertyValue = propertyInfo.GetValue(source);
+
+            ((IDictionary<string, object?>)dataShapedObject).Add(propertyInfo.Name, propertyValue);
+        }
+
+        return dataShapedObject;
+    }
+}
