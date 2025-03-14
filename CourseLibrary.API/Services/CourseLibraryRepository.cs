93dcc1a6ff3b78ab6844dec20f312e14145a1c43
Sina Abbaszadeh <sinaabszde83@gmail.com>
2025-03-14 15:25:50 +0330

    feat: add a reusable IQueryable helper extension method to apply sorting

--- CourseLibrary.API/Services/CourseLibraryRepository.cs
+++ CourseLibrary.API/Services/CourseLibraryRepository.cs
@@ -1,6 +1,7 @@
 ﻿using CourseLibrary.API.DbContexts;
 using CourseLibrary.API.Entities;
 using CourseLibrary.API.Helpers;
+using CourseLibrary.API.Models;
 using CourseLibrary.API.ResourceParameters;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,10 +10,13 @@ namespace CourseLibrary.API.Services;
 public class CourseLibraryRepository : ICourseLibraryRepository 
 {
     private readonly CourseLibraryContext _context;
+    private readonly IPropertyMappingService _propertyMappingService;
 
-    public CourseLibraryRepository(CourseLibraryContext context)
+    public CourseLibraryRepository(CourseLibraryContext context, IPropertyMappingService propertyMappingService)
     {
         _context = context ?? throw new ArgumentNullException(nameof(context));
+        _propertyMappingService =
+            propertyMappingService ?? throw new ArgumentNullException(nameof(propertyMappingService));
     }
 
     public void AddCourse(Guid authorId, Course course)
@@ -151,10 +155,11 @@ public class CourseLibraryRepository : ICourseLibraryRepository
 
         if (!string.IsNullOrWhiteSpace(authorsResourceParameters.OrderBy))
         {
-            if (authorsResourceParameters.OrderBy.Equals("Name", StringComparison.OrdinalIgnoreCase))
-            {
-                collection = collection.OrderBy(a => a.FirstName).ThenBy(a => a.LastName);
-            }
+            // Get property mapping dictionary
+            var authorPropertyMappingDictionary = _propertyMappingService.GetPropertyMapping<Author, AuthorDto>();
+
+
+            collection = collection.ApplySort(authorsResourceParameters.OrderBy, authorPropertyMappingDictionary);
         }
 
         return await PagedList<Author>.CreateAsync(
